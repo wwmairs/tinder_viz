709e45eea1aa95b83025ae8c1a684e65b4450ce4
wwmairs <wwmairs@gmail.com>
2019-10-20 15:39:40 -0600

    switched to firestore; got random working

--- swipe_logic_app/main.py
+++ swipe_logic_app/main.py
@@ -1,61 +1,84 @@
 import firebase_admin
-from firebase_admin import db
+from firebase_admin import firestore
 from flask import Flask, jsonify, redirect, render_template, request, url_for
+from random import randrange
 
 app = Flask(__name__)
 
-firebase_admin.initialize_app(options={
-    'databaseURL': 'https://swipe-logic.firebaseio.com'
-})
+firebase_admin.initialize_app()
 
-BIOS = db.reference('bios')
+db = firestore.client()
 
-@app.route('/')
-def index():
-    return 'Hello'
+BIOS = db.collection(u'bios')
 
-@app.route('/bios', methods=['POST'])
-def add_bio():
-    req = request.json
-    bio = BIOS.push(req)
-    return jsonify({'id': bio.key}), 201
-
-#@app.route('/bios/')
-#def random_bio():
-#    bio = 
+@app.route('/bios/')
+def random_bio():
+    bio = get_random_bio()  
+    return render_template('bio.html', bio=bio)
 
 @app.route('/bios/<id>')
 def get_bio(id):
     bio = bio_or_404(id)
-    key = bio.key
-    return render_template('bio.html', bio=bio.get(), key=key)
+    return render_template('bio.html', bio=bio)
 
 
 @app.route('/bios/<id>/json')
 def get_bio_json(id):
-    return jsonify(bio_or_404(id).get())
+    return jsonify(bio_or_404(id))
 
 @app.route('/bios/<id>/upvote', methods=['POST'])
 def upvote_bio(id):
-    bio = bio_or_404(id)
+    bio = bio_ref_or_404(id)
+    old_val = bio.get().to_dict()['upvotes']
     bio.update({
-        'upvotes' : bio.get()['upvotes'] + 1,
+        'upvotes' : old_val + 1,
     })
     return redirect(url_for('get_bio', id=id))
 
 @app.route('/bios/<id>/downvote', methods=['POST'])
 def downvote_bio(id):
-    bio = bio_or_404(id)
+    bio = bio_ref_or_404(id)
+    old_val = bio.get().to_dict()['downvotes']
     bio.update({
-        'downvotes' : bio.get()['downvotes'] + 1,
+        'downvotes' : old_val + 1,
     })
     return redirect(url_for('get_bio', id=id))
 
-def bio_or_404(id):
-    bio = BIOS.child(id)
-    if not bio:
+def bio_ref_or_404(id):
+    doc_ref = BIOS.document(id)
+    if not doc_ref:
         abort(404)
-    return bio
+    return doc_ref
 
-#def get_random_bio():
-#    bio = BIOS.
+def bio_or_404(id):
+    doc_ref = BIOS.document(id)
+    doc = doc_ref.get()
+    if not doc:
+        abort(404)
+    return ref_to_dict(doc)
+
+def ref_to_dict(ref):
+    ref_dict = ref.to_dict()
+    ref_dict['id'] = ref.id
+    return ref_dict
+
+def get_random_bio():
+    random_id = get_random_id()
+    random_ref = BIOS.where('random', '<=', random_id)\
+                     .limit(1)
+
+    try:
+        response = next(random_ref.stream())
+    except (StopIteration):
+        random_ref = BIOS.where('random', '>=', random_id)\
+                         .limit(1)
+        response = next(random_ref.stream())
+    return ref_to_dict(response)
+
+
+def get_random_id():
+    chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    random_id = ''
+    for i in range(20):
+       random_id += chars[randrange(len(chars))] 
+    return random_id
